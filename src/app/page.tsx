93f32026b11f6509ab925a4be36210d4ff131967
VasylPopaz <pafos1997@gmail.com>
2024-08-31 18:11:38 +0200

    add fetch snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,15 @@
-const Home = () => {
-  return <div>Home</div>;
+import { db } from "@/db";
+
+const Home = async () => {
+  const snippets = await db.snippet.findMany();
+
+  const renderedSnippets = snippets.map((snippet) => (
+    <li key={snippet.id}>{snippet.title}</li>
+  ));
+  return (
+    <div>
+      <ul>{renderedSnippets}</ul>
+    </div>
+  );
 };
 export default Home;
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -14,6 +14,7 @@ const SnippetCreatePage = () => {
       },
     });
     console.log(snippet);
+    
     redirect("/");
   };
 
