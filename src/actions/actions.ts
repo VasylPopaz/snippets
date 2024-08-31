9e989dd906d53dcd6d457984cfd079866d1c5fb3
VasylPopaz <pafos1997@gmail.com>
2024-08-31 20:42:42 +0200

    add editSnippet action

---
+++ src/actions/actions.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export const editSnippet = async (id: number, code: string) => {
+  await db.snippet.update({
+    where: { id },
+    data: {
+      code,
+    },
+  });
+  redirect(`/snippets/${id}`);
+};
