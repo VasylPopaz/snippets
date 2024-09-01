090e154dd2e0a2be4f0a0656599d5682cb186362
VasylPopaz <pafos1997@gmail.com>
2024-09-01 22:39:15 +0200

    add createSnippet action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -3,6 +3,38 @@
 import { db } from "@/db";
 import { redirect } from "next/navigation";
 
+export const createSnippet = async (
+  formsState: { message: string },
+  formData: FormData
+) => {
+  try {
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return { message: "Title must be longer" };
+    }
+
+    if (typeof code !== "string" || code.length < 15) {
+      return { message: "Code must be longer" };
+    }
+
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return { message: error.message };
+    } else {
+      return { message: "Something went wrong..." };
+    }
+  }
+  redirect("/");
+};
+
 export const editSnippet = async (id: number, code: string) => {
   await db.snippet.update({
     where: { id },
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,24 +1,13 @@
-import { db } from "@/db";
-import { redirect } from "next/navigation";
+"use client";
+
+import { createSnippet } from "@/actions";
+import { useFormState } from "react-dom";
 
 const SnippetCreatePage = () => {
-  const createSnippet = async (formData: FormData) => {
-    "use server";
-    const title = formData.get("title") as string;
-    const code = formData.get("code") as string;
-
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code,
-      },
-    });
-
-    redirect("/");
-  };
+  const [formState, action] = useFormState(createSnippet, { message: "" });
 
   return (
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold py-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -30,7 +19,6 @@ const SnippetCreatePage = () => {
             name="title"
             id="title"
             className="border-2 rounded p-2 w-full outline-none focus-visible:border-purple-500"
-            required
           />
         </div>
         <div className="flex gap-4">
@@ -43,6 +31,13 @@ const SnippetCreatePage = () => {
             className="border-2 rounded p-2 w-full outline-none focus-visible:border-purple-500"
           />
         </div>
+
+        {formState.message ? (
+          <div className="mt-2 p-2 bg-red-200 border border-red-400 rounded ">
+            {formState.message}
+          </div>
+        ) : null}
+
         <button
           type="submit"
           className="rounded p-2 bg-purple-500 text-white hover:bg-purple-800 focus-visible:bg-purple-800"
